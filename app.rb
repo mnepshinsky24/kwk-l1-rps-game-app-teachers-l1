abbe051efae954e60bbe12f20635d761012c71e7
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-08-03 15:07:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
+    
     # Your app code goes here
     # Don't worry about this UNTIL you've passed all the tests!
     erb :rps_game
   end
 end
+
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -2,7 +2,9 @@ class RPSGame
 
   # Write your code here!
 
-
+def initialize(play)
+  
+  raise PlayTypeError 
 
   class PlayTypeError < StandardError
     # Don't be alarmed! This is a class set inside a class, but works just fine in Ruby
@@ -10,3 +12,5 @@ class RPSGame
   end
 
 end
+
+play1 = RPSGame.new
